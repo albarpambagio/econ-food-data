9932e0c591d971a5c2e17537f8361ab5cd7ab369
albarpambagio <albarpambagio@gmail.com>
2023-12-20 08:21:14 +0700

    adding some tidy data concepts and handling mixed types

--- technical/exploration.py
+++ technical/exploration.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 #tidy data
 #TODO apply basic tidy data concept (cleansing) ✅
-#TODO handle mixed data type
+#TODO handle mixed data type ✅ (by set low memeory to false when importing the data)
 '''
 mixed_type_columns = data.iloc[:, [4, 5, 12, 13]]
 print(mixed_type_columns.applymap(type).nunique())
@@ -14,7 +14,7 @@ print(mixed_type_columns.applymap(type).nunique())
 #TODO check duplicated ✅
 #TODO add comments/docstring
 #TODO handle missing values 
-#TODO apply some extended tidy data structure (if necessary)
+#TODO apply some extended tidy data structure (if necessary) (pivot/melt) 🧑‍🍳
 #TODO handle outliers
 #TODO data transformation https://aeturrell.github.io/coding-for-economists/data-transformation.html
 #TODO data analysis, stats work (variable analysis, correlation, hypothesis testing), & visualization
@@ -42,7 +42,7 @@ profile = ProfileReport(
 profile.to_file("your_report.html")
 '''
 
-data = pd.read_csv(csv_file_path)
+data = pd.read_csv(csv_file_path, low_memory=False)
 data['price'] = pd.to_numeric(data['price'], errors='coerce')
 
 data['date'] = pd.to_datetime(data['date'], errors='coerce', format='%Y-%m-%d')
@@ -55,21 +55,38 @@ data = data.set_index('date')
 
 
 #data_uncluttered = data.drop(['admin1', 'admin2', 'latitude', 'longitude', 'usdprice'], axis=1, inplace=True)
-data_uncluttered = data.drop(['admin1', 'admin2', 'latitude', 'longitude', 'usdprice'], axis=1).copy()
+data = data.drop(['admin1', 'admin2', 'latitude', 'longitude', 'usdprice'], axis=1).copy()
 
 data = data.rename(columns={'data_year': 'year', 'data_month': 'month', 'data_day': 'day'})
 
 categorical_columns = ['market', 'category', 'commodity', 'unit', 'priceflag', 'pricetype', 'currency']
 data[categorical_columns] = data[categorical_columns].astype('category')
 
-
-data_info = data.info()
-data_duplicate= data.duplicated().sum()
-print(f"Number of duplicated rows: {data_duplicate}")
-
-# data analysis
+data['priceflag'] = data['priceflag'].str.replace('#', '').astype('category')
+data['pricetype'] = data['pricetype'].str.replace('#', '').astype('category')
 
 
+
+
+
+#data['priceflag'] = data['priceflag'].astype(str)
+#data_info = data.info()
+#data_duplicate= data.duplicated().sum()
+#data_mixed = data['priceflag'].isnull().sum()
+#data_mixed_info = data['priceflag'].dtypes()
+#print(f"Number of duplicated rows: {data_duplicate}")
+data_info= data.columns
+print(data_info)
+# Clean special characters
+
+#print(data['priceflag'].unique())
+#print(data['pricetype'].unique())
+
+# tidy data
+# melt
+tidy_data_one = data.melt(id_vars=['market', 'year', 'month', 'day'], var_name='variables', value_vars=['category', 
+  'commodity', 'unit', 'priceflag', 'pricetype', 'currency', 'price'])
+print(tidy_data_one.head(20))
 
 
 
